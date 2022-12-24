529b83a61660428577412b4092a15901381ef84d
Tiegertropfen119 <46197002+Tiegertropfen119-0001@users.noreply.github.com>
2022-12-24 01:04:15 +0100

    Add files via upload

---
+++ Schreibmaschichen/Form1.Designer.vb
@@ -0,0 +1,218 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(6, 19)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(225, 70)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(218, 92)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(13, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "0"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(316, 159)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Live Type"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button4)
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Controls.Add(Me.Button1)
+        Me.GroupBox2.Controls.Add(Me.RichTextBox1)
+        Me.GroupBox2.Location = New System.Drawing.Point(18, 177)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(310, 125)
+        Me.GroupBox2.TabIndex = 3
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Text"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(231, 46)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Center"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(231, 67)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Big"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(231, 17)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Normal"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(6, 19)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(219, 96)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(253, 308)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Push"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Button6)
+        Me.GroupBox3.Controls.Add(Me.Button5)
+        Me.GroupBox3.Location = New System.Drawing.Point(334, 17)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(132, 314)
+        Me.GroupBox3.TabIndex = 4
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Barcodes"
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(6, 19)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(120, 23)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "UPC_A Int"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(6, 48)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(120, 23)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "CODE93 Str & Int"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(172, 308)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(75, 23)
+        Me.Button7.TabIndex = 5
+        Me.Button7.Text = "Sleep"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(91, 308)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(75, 23)
+        Me.Button8.TabIndex = 6
+        Me.Button8.Text = "Wake"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(478, 369)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.MaximumSize = New System.Drawing.Size(9999, 9999)
+        Me.MinimumSize = New System.Drawing.Size(300, 300)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+End Class
---
+++ Schreibmaschichen/Form1.vb
@@ -0,0 +1,157 @@
+﻿Imports System.IO.Ports
+Imports System.Threading
+
+Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim MyCOMPort As SerialPort
+        MyCOMPort = New SerialPort()
+        MyCOMPort.PortName = "COM3"
+        MyCOMPort.BaudRate = 57600
+        MyCOMPort.Parity = Parity.None
+        MyCOMPort.DataBits = 8
+        MyCOMPort.StopBits = StopBits.One
+        MyCOMPort.Open()
+        MyCOMPort.Write("1")
+        MyCOMPort.Close()
+        MyCOMPort.Open()
+        MyCOMPort.Write(TextBox1.Text)
+        MyCOMPort.Close()
+
+        TextBox1.Text = ""
+        Label1.Text += 0.5
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim MyCOMPort As SerialPort
+        MyCOMPort = New SerialPort()
+        MyCOMPort.PortName = "COM3"
+        MyCOMPort.BaudRate = 57600
+        MyCOMPort.Parity = Parity.None
+        MyCOMPort.DataBits = 8
+        MyCOMPort.StopBits = StopBits.One
+        MyCOMPort.Open()
+        MyCOMPort.Write("2")
+
+        MyCOMPort.Close()
+        MyCOMPort.Open()
+        MyCOMPort.Write(RichTextBox1.Text)
+        MyCOMPort.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim MyCOMPort As SerialPort
+        MyCOMPort = New SerialPort()
+        MyCOMPort.PortName = "COM3"
+        MyCOMPort.BaudRate = 57600
+        MyCOMPort.Parity = Parity.None
+        MyCOMPort.DataBits = 8
+        MyCOMPort.StopBits = StopBits.One
+        MyCOMPort.Open()
+        MyCOMPort.Write("3")
+
+        MyCOMPort.Close()
+        MyCOMPort.Open()
+        MyCOMPort.Write(RichTextBox1.Text)
+        MyCOMPort.Close()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim MyCOMPort As SerialPort
+        MyCOMPort = New SerialPort()
+        MyCOMPort.PortName = "COM3"
+        MyCOMPort.BaudRate = 57600
+        MyCOMPort.Parity = Parity.None
+        MyCOMPort.DataBits = 8
+        MyCOMPort.StopBits = StopBits.One
+        MyCOMPort.Open()
+        MyCOMPort.Write("4")
+
+        MyCOMPort.Close()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim MyCOMPort As SerialPort
+        MyCOMPort = New SerialPort()
+        MyCOMPort.PortName = "COM3"
+        MyCOMPort.BaudRate = 57600
+        MyCOMPort.Parity = Parity.None
+        MyCOMPort.DataBits = 8
+        MyCOMPort.StopBits = StopBits.One
+        MyCOMPort.Open()
+        MyCOMPort.Write("5")
+
+        MyCOMPort.Close()
+        MyCOMPort.Open()
+        MyCOMPort.Write(RichTextBox1.Text)
+        MyCOMPort.Close()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim MyCOMPort As SerialPort
+        MyCOMPort = New SerialPort()
+        MyCOMPort.PortName = "COM3"
+        MyCOMPort.BaudRate = 57600
+        MyCOMPort.Parity = Parity.None
+        MyCOMPort.DataBits = 8
+        MyCOMPort.StopBits = StopBits.One
+        MyCOMPort.Open()
+        MyCOMPort.Write("6")
+
+        MyCOMPort.Close()
+        MyCOMPort.Open()
+        MyCOMPort.Write(RichTextBox1.Text)
+        MyCOMPort.Close()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim MyCOMPort As SerialPort
+        MyCOMPort = New SerialPort()
+        MyCOMPort.PortName = "COM3"
+        MyCOMPort.BaudRate = 57600
+        MyCOMPort.Parity = Parity.None
+        MyCOMPort.DataBits = 8
+        MyCOMPort.StopBits = StopBits.One
+        MyCOMPort.Open()
+        MyCOMPort.Write("7")
+
+        MyCOMPort.Close()
+        MyCOMPort.Open()
+        MyCOMPort.Write(RichTextBox1.Text)
+        MyCOMPort.Close()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim MyCOMPort As SerialPort
+        MyCOMPort = New SerialPort()
+        MyCOMPort.PortName = "COM3"
+        MyCOMPort.BaudRate = 57600
+        MyCOMPort.Parity = Parity.None
+        MyCOMPort.DataBits = 8
+        MyCOMPort.StopBits = StopBits.One
+        MyCOMPort.Open()
+        MyCOMPort.Write("98")
+
+        MyCOMPort.Close()
+        MsgBox("Please wait 3000ms")
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Dim MyCOMPort As SerialPort
+        MyCOMPort = New SerialPort()
+        MyCOMPort.PortName = "COM3"
+        MyCOMPort.BaudRate = 57600
+        MyCOMPort.Parity = Parity.None
+        MyCOMPort.DataBits = 8
+        MyCOMPort.StopBits = StopBits.One
+        MyCOMPort.Open()
+        MyCOMPort.Write("99")
+
+        MyCOMPort.Close()
+        MsgBox("Please wait 1500ms")
+    End Sub
+End Class
